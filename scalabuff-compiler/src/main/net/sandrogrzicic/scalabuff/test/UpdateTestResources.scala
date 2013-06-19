71ef706a7cf68d7db0c1f75f2049e972fa97eb63
SandroGrzicic <seriousworm@gmail.com>
2013-06-20 01:24:08 +0200

    Fixed the generated UsesImportedPackage in UpdateTestResources.

--- scalabuff-compiler/src/main/net/sandrogrzicic/scalabuff/test/UpdateTestResources.scala
+++ scalabuff-compiler/src/main/net/sandrogrzicic/scalabuff/test/UpdateTestResources.scala
@@ -1,11 +1,13 @@
 package net.sandrogrzicic.scalabuff.test
 
-import net.sandrogrzicic.scalabuff.compiler.{Generator, Parser, Node, buffString}
+import net.sandrogrzicic.scalabuff.compiler._
 import java.io.File.{separator => SEP}
 import java.io._
+import scala.Some
 
 /**
  * A small program which updates the test resources (ScalaBuff outputs).
+ *
  * @author Sandro Gržičić
  */
 
@@ -54,7 +56,11 @@ object UpdateTestResources extends App {
 
 			parsedOption.foreach { parsed =>
 				// if we have a valid parsing tree, generate a Scala proto class.
-        val generated = Generator(parsed, file.getName, Map())
+
+        // for now, this is hard-coded.
+        val importedSymbols = Map("PackageTest" -> ImportedSymbol("nested", false))
+
+        val generated = Generator(parsed, file.getName, importedSymbols)
 				val generatedPath = testDir + generated.path + generated.file + ".scala"
 
         new File(testDir + generated.path).mkdirs()
